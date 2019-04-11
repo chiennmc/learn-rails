83669ee1862d2fcaf11f6d2674f3f13cb05614d5
chiennm <ngominhchien96@gmail.com>
2019-04-11 14:03:57 +0700

    Signup done

--- example/app/controllers/users_controller.rb
+++ example/app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -27,15 +26,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render "new"
+        # format.html { render :new }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
+    # end
   end
 
   # PATCH/PUT /users/1
